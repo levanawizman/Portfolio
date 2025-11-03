a4d407df89a5df07206a1d50cf12f87577b5eef9
levanawizman <Levana.WIZMAN@ortmontreuil.fr>
2025-11-03 13:29:17 +0100

    Initial commit: Portfolio React + TypeScript + Vite

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <main className="w-full min-h-screen flex items-center justify-center bg-white">
+      <h1 className="text-4xl font-extrabold tracking-tight text-sky-600">
+        Portfolio
+      </h1>
+    </main>
+  )
+}
+
+export default App
